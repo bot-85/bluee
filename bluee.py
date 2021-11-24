5e07da34c52db12ced20ade1737953fc4153f1b7
BINTANG-XD <94216075+bot-85@users.noreply.github.com>
2021-11-24 21:54:20 +0700

    Update bluee.py

--- bluee.py
+++ bluee.py
@@ -144,7 +144,7 @@ def moch_yayan():
     print '___________________________________________________________\n';time.sleep(0.03)
     print ' (\033[0;92m•\033[0m) USER NAME   : %s'%(nama);time.sleep(0.03)
     print ' (\033[0;94m•\033[0m) IP phone    : %s'%(IP)
-    print ' (\033[0;92m•\033[0m) Status      : Premium 
+    print(" [*] Status     : %sPremium Sampai dajjal keluar%s"%(H,N))
     print '___________________________________________________________\n';time.sleep(0.03)
     print ' [%s01%s] DUMP ID DARI TEMAN'%(U,P,);time.sleep(0.03)
     print ' [%s02%s] DUMP ID DARI PUBLIK'%(U,P);time.sleep(0.03)

e9b077c0671a1bdd9a6d6adc214de6fe32d8e2b1
BINTANG-XD <94216075+bot-85@users.noreply.github.com>
2021-11-24 22:18:11 +0700

    Update bluee.py

--- bluee.py
+++ bluee.py
@@ -88,10 +88,10 @@ def tod():
         time.sleep(1)
 
 # LO KONTOL
-logo = ''' \033[0;92m __  __        __   __        ____
- \033[0;92m \ \/ / ___   /  |/  / ___   / __/ © \x1b[1;92m|| BY : Bintang-XD
- \033[0;93m  \  / /___/ / /|_/ / /___/ / _/     \x1b[1;93m|| FB : Bintang Tzy
- \033[0;93m  /_/       /_/  /_/       /_/  \x1b[1;92mv6.5 \033[0m|| YT : BINTANG XD'''
+logo = ''' \033[0;92m ____        __   __        ____
+ \033[0;92m  / _ )  ___    /  |/  /  / __/ © \x1b[1;92m|| BY : Bintang-XD
+ \033[0;93m / _  / /___/  / /|_/ /  / _/     \x1b[1;93m|| FB : Bintang Tzy
+ \033[0;93m/___ /        /_/  /_/  /_/  \x1b[1;92mv6.5 \033[0m|| YT : BINTANG XD'''
 lo_ngentod = '1714009362122228'
 # crack selesai
 def hasil(ok,cp):
